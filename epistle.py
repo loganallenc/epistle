08d90e7825e351dfaa1eeaf6b4af523b03108719
loganfynne <devnull@localhost>
2011-02-22 17:46:58 -0500

    Changed the code to convert the printed number of messages into an integer. Prints the most recent 50 messages.

--- epistle.py
+++ epistle.py
@@ -7,14 +7,22 @@ import imaplib, email, re
 user = raw_input('What is your username: ')
 password = raw_input('What is your password: ')
 
-mail = imaplib.IMAP4_SSL('imap.gmail.com', 993)
-auth, logged = mail.login(user, password)
-print (auth, logged)
+mail = imaplib.IMAP4_SSL('imap.gmail.com', 993) #Connects to Gmail.
+auth, logged = mail.login(user, password) #Logs in to Gmail.
+print (auth, logged) #Outputs success or failure of login.
 
 count = mail.select('Inbox')
-print ('Inbox: ', count[1])
-number = count[1]
-
+print 'Inbox: ', count[1]
+numinbox = re.split('', str(count[1]))
+numinbox = '0-9'.join(numinbox)
+number = len(numinbox)
+x = 0
+addto = []
+while x < number: #Converts number of items in the inbox into an 'int' object.
+	if numinbox[x].isdigit(): addto.append(numinbox[x])
+	x = x + 1
+numinbox = ''.join(addto)
+numinbox = int(numinbox)
 
 messages = mail.status('Inbox', '(MESSAGES)')
 print (messages)
@@ -23,7 +31,7 @@ print (unread)
 recent = mail.status('Inbox', '(RECENT)')
 print (recent)
 
-for x in range(1,20):
+for x in range((numinbox - 50),numinbox):
 	resp, data = mail.FETCH(x, '(RFC822)')
 	msg = HeaderParser().parsestr(data[0][1])
 	print '\n\n\nFrom: ', msg['From']
@@ -32,19 +40,19 @@ for x in range(1,20):
 	
 	mailitem = email.message_from_string(data[0][1])
  
-	for part in mailitem.walk():
+	for mailpart in mailitem.walk():
 		#print 'Content-Type:', part.get_content_type()
 		#print 'Main Content:', part.get_content_maintype()
 		#print 'Sub Content:', part.get_content_subtype()
 		# multipart are just containers, so we skip them
-		if part.get_content_maintype() == 'multipart':
+		if mailpart.get_content_maintype() == 'multipart':
 			continue
  
   		# we are interested only in the simple text messages
-		if part.get_content_subtype() != 'plain':
+		if mailpart.get_content_subtype() != 'plain':
 			continue
  
-		payload = part.get_payload()
+		payload = mailpart.get_payload()
   		print payload
  
 
