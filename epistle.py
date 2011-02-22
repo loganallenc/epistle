af72b636ad6600c2fa32e2bf167ebacc9f8d8115
loganfynne <devnull@localhost>
2011-02-22 13:38:40 -0500

    Added functionality for Subject, To, From, and text mail content. Only IMAP, no SMTP yet.

--- epistle.py
+++ epistle.py
@@ -1,8 +1,9 @@
 '''
 Written by: loganfynne
-Initial command-line mail function of Epistle. Can't read mail yet, or really do much of anything. Full email support, Twitter support, Facebook support, and a GUI needed.
 '''
-import imaplib, re
+from email.parser import HeaderParser
+import imaplib, email, re
+
 user = raw_input('What is your username: ')
 password = raw_input('What is your password: ')
 
@@ -11,7 +12,9 @@ auth, logged = mail.login(user, password)
 print (auth, logged)
 
 count = mail.select('Inbox')
-print (count[1])
+print ('Inbox: ', count[1])
+number = count[1]
+
 
 messages = mail.status('Inbox', '(MESSAGES)')
 print (messages)
@@ -19,5 +22,30 @@ unread = mail.status('Inbox', '(UNSEEN)')
 print (unread)
 recent = mail.status('Inbox', '(RECENT)')
 print (recent)
+
+for x in range(1,20):
+	resp, data = mail.FETCH(x, '(RFC822)')
+	msg = HeaderParser().parsestr(data[0][1])
+	print '\n\n\nFrom: ', msg['From']
+	print 'To: ', msg['To']
+	print 'Subject: ', msg['Subject'], '\n\n'
+	
+	mailitem = email.message_from_string(data[0][1])
+ 
+	for part in mailitem.walk():
+		#print 'Content-Type:', part.get_content_type()
+		#print 'Main Content:', part.get_content_maintype()
+		#print 'Sub Content:', part.get_content_subtype()
+		# multipart are just containers, so we skip them
+		if part.get_content_maintype() == 'multipart':
+			continue
+ 
+  		# we are interested only in the simple text messages
+		if part.get_content_subtype() != 'plain':
+			continue
+ 
+		payload = part.get_payload()
+  		print payload
+ 
 
 mail.logout()
