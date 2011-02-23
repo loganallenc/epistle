a2dac64f3682040373ba85852dee9cc6cadf9895
loganfynne <devnull@localhost>
2011-02-23 13:18:29 -0500

    Logs in and connects to SMTP server. Will add preliminary support for sending mail later.

--- epistle.py
+++ epistle.py
@@ -2,18 +2,21 @@
 Written by: loganfynne
 '''
 from email.parser import HeaderParser
-import imaplib, email, re
+import imaplib, smtplib, email, re
 
 user = raw_input('What is your username: ')
 password = raw_input('What is your password: ')
 
-mail = imaplib.IMAP4_SSL('imap.gmail.com', 993) #Connects to Gmail.
-auth, logged = mail.login(user, password) #Logs in to Gmail.
+imapmail = imaplib.IMAP4_SSL('imap.gmail.com', 993) #Connects to Gmail.
+auth, logged = imapmail.login(user, password) #Logs in to Gmail.
+print (auth, logged) #Outputs success or failure of login.
+smtpmail = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+auth, logged = smtpmail.login(user, password)
 print (auth, logged) #Outputs success or failure of login.
 
-count = mail.select('Inbox')
-print 'Inbox: ', count[1]
-numinbox = re.split('', str(count[1]))
+selectlabel = imapmail.select('Inbox')
+print 'Inbox: ', selectlabel[1]
+numinbox = re.split('', str(selectlabel[1]))
 numinbox = '0-9'.join(numinbox)
 number = len(numinbox)
 x = 0
@@ -24,15 +27,17 @@ while x < number: #Converts number of items in the inbox into an 'int' object.
 numinbox = ''.join(addto)
 numinbox = int(numinbox)
 
-messages = mail.status('Inbox', '(MESSAGES)')
-print (messages)
-unread = mail.status('Inbox', '(UNSEEN)')
-print (unread)
-recent = mail.status('Inbox', '(RECENT)')
-print (recent)
+messages = imapmail.status('Inbox', '(MESSAGES)')
+print 'Total: ', messages
+unread = imapmail.status('Inbox', '(UNSEEN)')
+print 'Unread: ', unread
+recent = imapmail.status('Inbox', '(RECENT)')
+print 'Recent: ', recent
+status, email_ids = imapmail.search(None, '(UNSEEN)')
+print email_ids
 
 for x in range((numinbox - 50),numinbox):
-	resp, data = mail.FETCH(x, '(RFC822)')
+	resp, data = imapmail.FETCH(x, '(RFC822)')
 	msg = HeaderParser().parsestr(data[0][1])
 	print '\n\n\nFrom: ', msg['From']
 	print 'To: ', msg['To']
@@ -41,19 +46,18 @@ for x in range((numinbox - 50),numinbox):
 	mailitem = email.message_from_string(data[0][1])
  
 	for mailpart in mailitem.walk():
-		#print 'Content-Type:', part.get_content_type()
-		#print 'Main Content:', part.get_content_maintype()
-		#print 'Sub Content:', part.get_content_subtype()
+		#print 'Content-Type:', mailpart.get_content_type()
+		#print 'Main Content:', mailpart.get_content_maintype()
+		#print 'Sub Content:', mailpart.get_content_subtype()
 		# multipart are just containers, so we skip them
 		if mailpart.get_content_maintype() == 'multipart':
 			continue
- 
-  		# we are interested only in the simple text messages
+
+		# we are interested only in the simple text messages
 		if mailpart.get_content_subtype() != 'plain':
 			continue
  
 		payload = mailpart.get_payload()
   		print payload
- 
 
 mail.logout()
